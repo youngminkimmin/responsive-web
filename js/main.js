bf84a11fe3276a2e7ce99ae72ed2c024237b0ade
jang sung jo <jjag051@hanmail.net>
2021-11-19 16:38:29 +0900

    first

---
+++ js/main.js
@@ -0,0 +1,23 @@
+const mainVisualSlider = new Swiper("#mainVisual", {
+  autoplay: true,
+  speed: 1000,
+  effect: "fade",
+  loop: true,
+  pagination: {
+    el: "#mainVisual .pagination",
+    type: "bullets",
+    clickable: true,
+  },
+});
+
+const productSlider = new Swiper("#product .list", {
+  speed: 600,
+  slidesPerView: 3, //화면에 보여지는 갯수
+  slidesPerGroup: 3, //묶음
+  navigation: {
+    prevEl: "#product .prev",
+    nextEl: "#product .next",
+  },
+});
+
+//f(x,y) = x+y*3; f(3,5) = 18
