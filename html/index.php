bdff786e700c2c3d1c577c23d35de457eaba643a
jang sung jo <jjag051@hanmail.net>
2021-11-22 10:19:21 +0900

    11월22 일

---
+++ html/index.php
@@ -0,0 +1,145 @@
+<? include("../include/header.html"); ?>
+<main id="main">
+    <div id="mainVisual">
+        <ul class="swiper-wrapper">
+            <!-- li.visual$$*3 -->
+            <li class="visual01 swiper-slide"></li>
+            <li class="visual02 swiper-slide"></li>
+            <li class="visual03 swiper-slide"></li>
+        </ul>
+        <div class="slogan">
+            <p class="main">Return to forest</p>
+            <p class="sub">숲으로 돌아가는 종이, 네오포레</p>
+            <a href="">브랜드 스토리 자세히 보기<span class="material-icons"> east </span></a>
+            <div class="pagination"></div>
+        </div>
+    </div>
+    <!-- mainVisual end -->
+    <article id="product">
+        <div class="titleBox">
+            <h2>
+                네오포레의 다양한 <br />
+                제품을 만나보세요.
+            </h2>
+            <p>무림은 최고의 기술로 생활의 가치를더하고 있습니다.</p>
+        </div>
+        <div class="list">
+            <!-- ul>li*6>a>img[src="../images/main/slide_img_$.png"] -->
+            <ul class="swiper-wrapper">
+                <li class="swiper-slide">
+                    <img src="../images/main/slide_img_1.png" alt="" />
+                    <a href="">네오포레 CUP <span class="material-icons">east</span></a>
+                </li>
+                <li class="swiper-slide">
+                    <img src="../images/main/slide_img_2.png" alt="" />
+                    <a href="">네오포레 STRAW <span class="material-icons">east</span></a>
+                </li>
+                <li class="swiper-slide">
+                    <img src="../images/main/slide_img_3.png" alt="" />
+                    <a href="">네오포레 CUP <span class="material-icons">east</span></a>
+                </li>
+                <li class="swiper-slide">
+                    <img src="../images/main/slide_img_4.png" alt="" />
+                    <a href="">네오포레 CUP <span class="material-icons">east</span></a>
+                </li>
+                <li class="swiper-slide">
+                    <img src="../images/main/slide_img_5.png" alt="" />
+                    <a href="">네오포레 CUP <span class="material-icons">east</span></a>
+                </li>
+                <li class="swiper-slide">
+                    <img src="../images/main/slide_img_6.png" alt="" />
+                    <a href="">네오포레 CUP <span class="material-icons">east</span></a>
+                </li>
+            </ul>
+        </div>
+        <div class="btns">
+            <button class="btn prev"><span class="material-icons"> navigate_before </span></button>
+            <button class="btn next"><span class="material-icons"> navigate_next </span></button>
+        </div>
+    </article>
+    <!-- product end -->
+    <article id="news">
+        <div class="contents">
+            <div class="titleBox">
+                <h2>
+                    네오포레의 <br />
+                    다양한 소식을 만나보세요.
+                </h2>
+                <p>무림은 최고의 기술로 생활에 가치를 더하고 있습니다.</p>
+            </div>
+            <ul class="list">
+                <li>
+                    <a href="">
+                        <div class="img" style="background-image: url('../images/main/news01.jpg')"></div>
+                        <div class="txtBox">
+                            <h3>
+                                탈 플라스틱 종이컵 '네오포레' 개발을 하든지 말든지 내 알바 아니고 탈 플라스틱 종이컵 '네오포레' 개발을 하든지 말든지 내 알바 아니고 탈 플라스틱 종이컵
+                                '네오포레' 개발을 하든지 말든지
+                                내 알바 아니고 탈 플라스틱 종이컵 '네오포레' 개발을 하든지 말든지 내 알바 아니고 탈 플라스틱 종이컵 '네오포레' 개발을 하든지 말든지 내 알바 아니고
+                            </h3>
+                            <p class="date">2021.11.04</p>
+                            <p class="txt">무림의 친환경 경영 및 플라스틱을 대체하는 '네오포레' 시리즈(출처: 단디뉴스)</p>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div class="img" style="background-image: url('../images/main/news02.jpg')"></div>
+                        <div class="txtBox">
+                            <h3>탈 플라스틱 종이컵 '네오포레' 개발</h3>
+                            <p class="date">2021.11.04</p>
+                            <p class="txt">무림의 친환경 경영 및 플라스틱을 대체하는 '네오포레' 시리즈(출처: 단디뉴스)</p>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div class="img" style="background-image: url('../images/main/news03.jpg')"></div>
+                        <div class="txtBox">
+                            <h3>탈 플라스틱 종이컵 '네오포레' 개발</h3>
+                            <p class="date">2021.11.04</p>
+                            <p class="txt">무림의 친환경 경영 및 플라스틱을 대체하는 '네오포레' 시리즈(출처: 단디뉴스)</p>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+            <div class="btns center">
+                <a href="" class="btn large transition">네오포레 소식 더하기</a>
+            </div>
+        </div>
+    </article>
+    <!-- news end -->
+    <div id="cs">
+        <div class="contents">
+            <ul>
+                <li>
+                    <a href="">
+                        <div class="txtBox">
+                            <strong>온라인 문의</strong>
+                            <p>궁금하신 사항을 문의 하실 수 있습니다.</p>
+                        </div>
+                        <div class="btn">
+                            문의하기
+                            <span class="material-icons">east</span>
+                        </div>
+                    </a>
+                </li>
+                <li>
+                    <a href="">
+                        <div class="txtBox">
+                            <strong>담당자 정보 </strong>
+                            <p>업무 담당자를 확인 하실 수 있습니다.</p>
+                        </div>
+                        <div class="btn">
+                            정보확인
+                            <span class="material-icons">east</span>
+                        </div>
+                    </a>
+                </li>
+            </ul>
+        </div>
+    </div>
+    <!-- cs end -->
+</main>
+<!-- main end -->
+<? include("../include/footer.html"); ?>
